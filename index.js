b18470f9f77ac2ac173aa171f0186f02dbcce18d
Hariom Tomar <68967590+harrybhai123@users.noreply.github.com>
2023-02-16 14:47:01 +0530

    added

---
+++ index.js
@@ -0,0 +1,54 @@
+const Floor1AImg = document.getElementById("Floor1AImg")
+const FloorTom2B = document.getElementById("FloorTom2B")
+
+Floor1AImg.addEventListener("click", function () {
+    FloorTom2B.play()
+})
+
+
+const BassDrumImg = document.getElementById("BassDrumImg")
+const BassDrumAudio = document.getElementById("BassDrumAudio")
+
+BassDrumImg.addEventListener("click", function () {
+    BassDrumAudio.play()
+})
+
+
+const CrashCymbalImg = document.getElementById("CrashCymbalImg")
+const CrashCymbalAudio = document.getElementById("CrashCymbalAudio")
+
+CrashCymbalImg.addEventListener("click", function () {
+    CrashCymbalAudio.play()
+})
+
+
+const CymbalImg = document.getElementById("CymbalImg")
+const CymbalAudio = document.getElementById("CymbalAudio")
+
+CymbalImg.addEventListener("click", function () {
+    CymbalAudio.play()
+})
+
+
+const HiHatImg = document.getElementById("HiHatImg")
+const HiHatAudio = document.getElementById("HiHatAudio")
+
+HiHatImg.addEventListener("click", function () {
+    HiHatAudio.play()
+})
+
+
+const MediumTomImg = document.getElementById("MediumTomImg")
+const MediumTomAudio = document.getElementById("MediumTomAudio")
+
+MediumTomImg.addEventListener("click", function () {
+    MediumTomAudio.play()
+})
+
+
+const SmallTomImg = document.getElementById("SmallTomImg")
+const SmallTomAudio = document.getElementById("SmallTomAudio")
+
+SmallTomImg.addEventListener("click", function () {
+    SmallTomAudio.play()
+})
